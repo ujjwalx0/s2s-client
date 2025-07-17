f8fa65f1a609d9c4b82fbd008c82cd695ff27811
ujjwal <ujjwals346@gmail.com>
2025-07-17 23:46:29 +0530

    Added Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,14 @@
 import './globals.css';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
-
+import { Analytics } from '@vercel/analytics/next';
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body suppressHydrationWarning={true}>
         <Header />
         {children}
+        <Analytics />
         <Footer />
       </body>
     </html>
