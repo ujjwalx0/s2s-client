360b6dc8ca09e1990cf6156400c6a5c59b2c655f
ujjwal <ujjwals346@gmail.com>
2025-07-18 08:13:11 +0530

    Added Speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
@@ -9,6 +10,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <Header />
         {children}
         <Analytics />
+        <SpeedInsights />
         <Footer />
       </body>
     </html>
